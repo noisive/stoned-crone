61b1a965925be1d4a95055060ebbf0f3fb299a5a
William Warren <wwarren@oucs1263.otago.ac.nz>
2017-05-05 16:33:54 +1200

    fixed bugs in python parser

--- Python parser conceptProof/parse.py
+++ Python parser conceptProof/parse.py
@@ -5,31 +5,31 @@ import sys
 import fileinput
 
 def parse(text, target, endTarget):
-	targetIndex = 0
-	targLength = len(target)
-	currentlyMatching = False
-	doExtract = False
-	out = ""
-	for letter in text:
-		
-		if doExtract == True:
-			if letter == endTarget:
-				return out
-			else:
-				out+=letter
-		else:
-		
-			if letter == target[targetIndex]:
-				currentlyMatching = True
-				if targetIndex == targLength -1:
-					doExtract = True
-				targetIndex+=1
+    targetIndex = 0
+    targLength = len(target)
+    currentlyMatching = False
+    doExtract = False
+    out = ""
+    for letter in text:
+        
+        if doExtract == True:
+            if letter == endTarget:
+                return out
+            else:
+                out+=letter
+        else:
+        
+            if letter == target[targetIndex]:
+                currentlyMatching = True
+                if targetIndex == targLength -1:
+                    doExtract = True
+                targetIndex+=1
                 
-			else:
-				currentlyMatching = False
-				targetIndex = 0
-					
-	return None
+            else:
+                currentlyMatching = False
+                targetIndex = 0
+                    
+    return None
     
 data = {"10:00" : ["11:00", "lightgrey"]}
 dataLabels = ["Day of timetable", "start time","end time", "colour", "Class type", "Paper code", "Paper name", "Map url", "Stream", "Room code", "Room name", "Building"]
@@ -37,10 +37,14 @@ parseStartTags =["\"day\":", "\"start\":\"", "\"end\":\"","\"fcol\":\"", "\"info
 parseEndTags=[",","\"","\"","\"", "<","<","<","\"","<","<","<","<"]
 
 
-dataIndex = 0;
-dictKeyIndex = 1; # Start time is key
-currKey = ""
+
+
 for line in fileinput.input():
+    dataIndex = 0;
+    dictKeyIndex = 1; # Start time is key
+    currKey = ""
+    tempInfo = []
+    """
     parsedInfo = parse(line, parseStartTags[dataIndex], parseEndTags[dataIndex])
     
     if parsedInfo == None:
@@ -56,7 +60,26 @@ for line in fileinput.input():
             dataIndex = 0
         else:
             dataIndex += 1
-            
+    """
+
+    while True: #breaks if parsedInfo == None (no data)
+        parsedInfo = parse(line, parseStartTags[dataIndex], parseEndTags[dataIndex])
+        if parsedInfo == None:
+            break
+        
+        if dataIndex == dictKeyIndex:
+                currKey = parsedInfo
+        try:
+            data[currKey].append(parsedInfo)
+        except KeyError:
+            tempInfo.append(parsedInfo)
+    if dataIndex == len(dataLabels):
+        data[currKey] = tempInfo
+        break
+        dataIndex = 0
+    else:
+        dataIndex += 1
+    continue
     
 print data
 #testLine = raw_input()
