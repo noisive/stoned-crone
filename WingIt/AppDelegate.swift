36a077ba24f5f00142572b06ce93caa1c034e79a
William Warren <wwarren@oucs1442.otago.ac.nz>
2017-10-02 23:11:01 +1300

    Added notification setup function

--- WingIt/AppDelegate.swift
+++ WingIt/AppDelegate.swift
@@ -17,7 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         initParser();
+        // Register intent to use notifications
+        application.registerUserNotificationSettings(UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil))
+
         return true
     }
 
--- WingIt/Helper.swift
+++ WingIt/Helper.swift
@@ -9,7 +9,26 @@
 import Foundation
 
 
-// Call this function when each item is initialised to schedule a notification for that event.
-func setNotification (String time, String paper, String type, String date){
+// Call this function when each item is initialised to schedule a 
+// notification [minsBeforeNotification] minutes before that event.
+// @param the event struct to be used.
+func setNotification (event: Lesson){
     
+    let minsBeforeNotification = 15
+    
+    let dateFormatter = DateFormatter()
+    // Currently we don't have the date...
+    // DateFormatter.dateFormat = "yyyy-MM-dd'T'HH"
+    dateFormatter.dateFormat = "HHmm"
+    dateFormatter.timeZone = NSTimeZone.default
+    
+    let notificationTime = (event.startTime-1)*100+60-minsBeforeNotification
+    let localNotification = UILocalNotification()
+    localNotification.fireDate = dateFormatter.date(from: String(notificationTime))
+    // Message example: COSC345 Lecture coming up at 11
+    localNotification.alertBody = "\(event.code) \(event.type) coming up at \(event.startTime)"
+    localNotification.timeZone = NSTimeZone.default
+    
+    //set the notification
+    UIApplication.shared.scheduleLocalNotification(localNotification)
 }
