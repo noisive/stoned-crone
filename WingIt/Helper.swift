968ae92303e03ea57b0ecb7764cf82106e47de72
William Warren <wwarren@oucs1442.otago.ac.nz>
2017-10-02 22:00:52 +1300

    Added helper file

---
+++ WingIt/Helper.swift
@@ -0,0 +1,15 @@
+//
+//  Helper.swift
+//  WingIt
+//
+//  Created by William Warren on 10/2/17.
+//  Copyright © 2017 Noisive. All rights reserved.
+//
+
+import Foundation
+
+
+// Call this function when each item is initialised to schedule a notification for that event.
+func setNotification (String time, String paper, String type, String date){
+    
+}
