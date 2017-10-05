fb50d7af0fadd670cde4dbe32a4aa5b1a95b33a5
William Warren <wwarren@oucs1420.otago.ac.nz>
2017-10-05 13:29:05 +1300

    Updated to latest Xcode (9) requirements

--- WingIt/AppDelegate.swift
+++ WingIt/AppDelegate.swift
@@ -72,7 +72,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: - Core Data stack
     
-    @available(iOS 10.0, *)
+    @objc @available(iOS 10.0, *)
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -101,19 +101,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
     
     // iOS 9 and below
-    lazy var applicationDocumentsDirectory: URL = {
+    @objc lazy var applicationDocumentsDirectory: URL = {
         
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return urls[urls.count-1]
     }()
     
-    lazy var managedObjectModel: NSManagedObjectModel = {
+    @objc lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
         let modelURL = Bundle.main.url(forResource: "coreDataTestForPreOS", withExtension: "momd")!
         return NSManagedObjectModel(contentsOf: modelURL)!
     }()
     
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+    @objc lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
         // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
@@ -138,7 +138,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return coordinator
     }()
     
-    lazy var managedObjectContext: NSManagedObjectContext = {
+    @objc lazy var managedObjectContext: NSManagedObjectContext = {
         // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
         let coordinator = self.persistentStoreCoordinator
         var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
@@ -148,7 +148,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: - Core Data Saving support
     
-    func saveContext () {
+    @objc func saveContext () {
         
         if #available(iOS 10.0, *) {
             
