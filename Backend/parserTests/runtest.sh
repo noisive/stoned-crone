9ffb96000d751dbbcd3af5b5ee30b547530763a1
William <bluedrink9@gmail.com>
2018-04-05 13:50:13 +1200

    Fix bugs in runtest.sh, implement untested diff and return code

--- Backend/parserTests/runtest.sh
+++ Backend/parserTests/runtest.sh
@@ -29,9 +29,20 @@ diffCSVs() {
     for file in "TestOutputs/*"; do
         diff=`diff $file "TestComparisons/${file}"`
         # Remove old diffs before replacing
-        rm "TestDiffs/${file}.diff"
-        echo $diff >> "TestDiffs/${file}.diff"
+        rm "TestDiffs/${file}.diff" > /dev/null 2>&1
+        if [ $diff = "" ]; then
+            FAILED=true
+            echo $diff >> "TestDiffs/${file}.diff"
+        fi
     done
+    if [ $FAILED = true ]; then
+        echo "Some tests failed! Check TestDiffs for more."
+        exit 1
+    else
+        echo "All tests pass!"
+        exit 0
+    fi
 }
 
 createCSVs
+diffCSVs
