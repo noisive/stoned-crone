c787d3cc4ce5e2cb74193fdbdf3422bc40420243
William <bluedrink9@gmail.com>
2018-04-07 14:32:25 +1200

    Whitespace

--- WingItUITests/WingItUITests.swift
+++ WingItUITests/WingItUITests.swift
@@ -37,24 +37,27 @@ class WingItUITests: XCTestCase {
         }
         self.eVisionUsername = eVisionUsername
         self.eVisionPassword = eVisionPassword
+
+
+        // In UI tests it is usually best to stop immediately when a failure occurs. (They are time-expensive to ru)
         continueAfterFailure = false
-        
+
+
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.\
         // We send a command line argument to our app,
         // to enable it to reset its state
         //app.launchArguments.append("--uitesting")
-        
-        
+
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
 
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    
+
+
     func testingPlayground(){
         app.launch()
 
@@ -64,17 +67,17 @@ class WingItUITests: XCTestCase {
         dayviewElement.tap()
         dayviewElement.swipeRight()
         dayviewElement.tap()
-        
+
         let thursdayNavigationBar = app.navigationBars["Thursday"]
         thursdayNavigationBar.tap()
-        
+
         let staticText = thursdayNavigationBar.staticTexts["05/10"]
         staticText.tap()
         staticText.tap()
         staticText.tap()
         dayviewElement.swipeLeft()
         thursdayNavigationBar.tap()
-        
+
     }
 
 
@@ -86,7 +89,7 @@ class WingItUITests: XCTestCase {
         print("hi")
 
         let webViewsQuery = app.webViews
-        
+
         let element2 = webViewsQuery.otherElements["Otago Student Administration - The University of Otago"].children(matching: .other).element(boundBy: 6)
         let textField = element2.children(matching: .textField).element
         // Need to wait
@@ -94,11 +97,11 @@ class WingItUITests: XCTestCase {
         textField.typeText(eVisionUsername)
         element2.children(matching: .secureTextField).element.typeText(eVisionPassword)
         app.typeText("\r")
-        
+
         //XCTAssertEqual(data.json, )
 
     }
-    
+
     func testLoginFresh(){
         app.launchArguments.append("--resetdata")
         app.launch()
@@ -113,21 +116,20 @@ class WingItUITests: XCTestCase {
         passwordSecureTextField.typeText(eVisionPassword)
         app.typeText("\r")
         XCUIApplication().navigationBars["Log in to eVision"].buttons["Cancel"].tap()
-        
+
         XCTAssertTrue(app.isDisplayingDay)
-        
+
     }
-    
+
     func testLoginUpdate(){
         testLoginFresh()
         app.launch()
         app.navigationBars["Friday"].children(matching: .button).element.tap()
         app.tables/*@START_MENU_TOKEN@*/.staticTexts["Update / Log in"]/*[[".cells.staticTexts[\"Update \/ Log in\"]",".staticTexts[\"Update \/ Log in\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        
         let usernameTextField = app.textFields["Username"]
         usernameTextField.tap()
         usernameTextField.typeText(eVisionUsername)
-        
+
         let passwordSecureTextField = app.secureTextFields["Password"]
         passwordSecureTextField.tap()
         passwordSecureTextField.typeText(eVisionPassword)
@@ -136,5 +138,4 @@ class WingItUITests: XCTestCase {
         XCTAssertTrue(app.isDisplayingDay)
         
     }
-    
 }
