2c5bf03a4a114976ebe66682e85247eb76476bcb
BlueDrink9 <bluedrink9@gmail.com>
2018-06-02 16:37:40 +1200

    Add testing cmd arg

--- WingItUITests/WingItUITests.swift
+++ WingItUITests/WingItUITests.swift
@@ -36,6 +36,7 @@ class WingItUITests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+        app.launchArguments.append("testing")
         // Put setup code here. This method is called before the invocation of each test method in the class.
         self.eVisionUsername = setUserFromEnv()
         self.eVisionPassword = setPassFromEnv()
@@ -48,7 +49,7 @@ class WingItUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.\
         // We send a command line argument to our app,
         // to enable it to reset its state
-        //app.launchArguments.append("--uitesting")
+        //app.launchArguments.append("uitesting")
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
 
@@ -69,7 +70,7 @@ class WingItUITests: XCTestCase {
     // Special webview window without any coverplate. For debugging webview login.
     func testLoginRaw(){
         // Argument to enter raw
-        app.launchArguments.append("--debugLogin")
+        app.launchArguments.append("debugLogin")
         app.launch()
         print("hi")
 
@@ -89,7 +90,7 @@ class WingItUITests: XCTestCase {
 
     // Test that a fresh login reaches a timetable.
     func testLoginFresh(){
-        app.launchArguments.append("--resetdata")
+        app.launchArguments.append("resetdata")
         app.launch()
         _ = app.launchArguments.popLast()
 
@@ -110,7 +111,7 @@ class WingItUITests: XCTestCase {
     }
     
     func testCancelButtonNotPresentFresh(){
-        app.launchArguments.append("--resetdata")
+        app.launchArguments.append("resetdata")
         app.launch()
         
         // Waits and checks for allow notifications alert.
