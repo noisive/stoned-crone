f7484a3cc7859ec3a97a914da948213886507f30
BlueDrink9 <bluedrink9@gmail.com>
2018-07-26 21:07:04 +1200

    Refactor out lesson object creation from CPP return into initialiser

--- WingIt/Lesson.swift
+++ WingIt/Lesson.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public struct Lesson {
+public class Lesson {
     
     var uid : CLong!
     var classID : String!
@@ -46,4 +46,29 @@ public struct Lesson {
         
     }
     
+    /* Converts a cpp eventString into a Lesson object */
+    convenience init(eventCSVStr: String){
+        let eventArr = eventCSVStr.components(separatedBy: "|")
+        // TODO these are sometimes force-unwrapped, causing crashes when the data is erroneous. Should be handled.
+        
+        //Define all data from CSV file and cast to correct data type.
+        let uid = CLong(eventArr[0])!
+        let dayNumber = Int(eventArr[1])!
+        let startTime = Int(eventArr[2])! - 8 // We start the day at 8 am
+        let duration = Int(eventArr[3])
+        let colour = eventArr[4]
+        let type = eventArr[5]
+        let paperCode = eventArr[6]
+        let paperName = eventArr[7]
+        let latitude = Double(eventArr[8])
+        let longitude = Double(eventArr[9])
+        let roomCode = eventArr[10]
+        let roomName = eventArr[11]
+        let building = eventArr[12]
+        let eventDateString = eventArr[13]
+        let eventDate = dateFromISOString(str: eventDateString)
+        
+        self.init(uid: uid, classID: paperCode, start: startTime, duration: duration!, colour: colour, code: paperCode, type: type, roomShort: roomCode, roomFull: roomName, building: building, paperName: paperName, day: dayNumber, eventDate: (eventDate)!, latitude: latitude!, longitude: longitude!)
+    }
+
 }
