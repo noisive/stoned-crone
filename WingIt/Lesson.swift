47ad04e347b10da54584dd09ef9defcea1527d29
BlueDrink9 <bluedrink9@gmail.com>
2018-07-26 21:22:55 +1200

    Ensure all values entering lesson init from Cpp are not nil

--- WingIt/Lesson.swift
+++ WingIt/Lesson.swift
@@ -26,6 +26,8 @@ public class Lesson {
     var latitude : Double!
     var longitude : Double!
     
+    static var nextUid = 101
+    
     init(uid: CLong, classID: String, start : Int, duration: Int, colour: String, code: String, type: String, roomShort: String, roomFull : String, building: String, paperName: String, day: Int, eventDate: Date, latitude: Double, longitude: Double) {
     //init(uid: CLong, classID: String, start : Int, length: Int, code: String, type: classType, roomShort: String, roomFull : String, paperName: String, day: Int, latitude: Double, longitude: Double) {
         self.uid = uid;
@@ -48,27 +50,72 @@ public class Lesson {
     
     /* Converts a cpp eventString into a Lesson object */
     convenience init(eventCSVStr: String){
-        let eventArr = eventCSVStr.components(separatedBy: "|")
+        var eventArr = eventCSVStr.components(separatedBy: "|")
         // TODO these are sometimes force-unwrapped, causing crashes when the data is erroneous. Should be handled.
         
+        // Prevent crashes when we try to access elements of the array. Instead, will just be garbage.
+        while eventArr.count < 14{
+            eventArr.append("0xCC")
+        }
+        
         //Define all data from CSV file and cast to correct data type.
-        let uid = CLong(eventArr[0])!
-        let dayNumber = Int(eventArr[1])!
-        let startTime = Int(eventArr[2])! - 8 // We start the day at 8 am
-        let duration = Int(eventArr[3])
+        // Var variables are optionals, any may return nil.
+        var uid = CLong(eventArr[0])
+        var dayNumber = Int(eventArr[1])
+        var startTime = Int(eventArr[2])
+        var duration = Int(eventArr[3])
         let colour = eventArr[4]
         let type = eventArr[5]
         let paperCode = eventArr[6]
-        let paperName = eventArr[7]
-        let latitude = Double(eventArr[8])
-        let longitude = Double(eventArr[9])
+        var paperName = eventArr[7]
+        var latitude = Double(eventArr[8])
+        var longitude = Double(eventArr[9])
         let roomCode = eventArr[10]
         let roomName = eventArr[11]
         let building = eventArr[12]
         let eventDateString = eventArr[13]
-        let eventDate = dateFromISOString(str: eventDateString)
+        var eventDate = dateFromISOString(str: eventDateString)
         
-        self.init(uid: uid, classID: paperCode, start: startTime, duration: duration!, colour: colour, code: paperCode, type: type, roomShort: roomCode, roomFull: roomName, building: building, paperName: paperName, day: dayNumber, eventDate: (eventDate)!, latitude: latitude!, longitude: longitude!)
+        let paperErrorName = "Error parsing paper: \"\(paperName)\". Time, date and details may be false. Please report this to the developers."
+        // Check if any optionals are nil, replace them with random stuff if so.
+        var failed = false
+        if uid == nil {
+            failed = true
+            uid = Lesson.nextUid
+            Lesson.nextUid += 1
+        }
+        if dayNumber == nil {
+            failed = true
+            dayNumber = 1
+        }
+        if startTime == nil {
+            failed = true
+            startTime = 1
+        }
+        if duration == nil {
+            failed = true
+            duration = 1
+        }
+        if latitude == nil {
+            failed = true
+            latitude = 175
+        }
+        if longitude == nil {
+            failed = true
+            longitude = 45
+        }
+        if eventDate == nil {
+            failed = true
+            eventDate = todaysDate()
+        }
+
+        if failed {
+            paperName = paperErrorName
+        }
+        
+        startTime = startTime! - 8 // We start the day at 8 am
+        
+        self.init(uid: uid!, classID: paperCode, start: startTime!, duration: duration!, colour: colour, code: paperCode, type: type, roomShort: roomCode, roomFull: roomName, building: building, paperName: paperName, day: dayNumber!, eventDate: (eventDate)!, latitude: latitude!, longitude: longitude!)
     }
 
 }
