eec9d0fcac359fbacea1f6aa919862425224eaf0
Unknown <bluedrink9@gmail.com>
2018-08-01 16:49:58 +1200

    Alter script to set version to BETA

--- setVersionFromGitTag.sh
+++ setVersionFromGitTag.sh
@@ -6,14 +6,14 @@
 # GIT_RELEASE_SHORT_VERSION=`git describe --tags | awk '{split($0,a,\"-\"); print a[1]}'`
 GIT_CURRENT_COMMIT_TAG=`git tag --points-at HEAD`
 # If the current head is untagged, is not a release. So specify beta, to prevent inconsistencies.
-if [ $GIT_CURRENT_COMMIT_TAG == "" ]; then
+if [[ "$GIT_CURRENT_COMMIT_TAG" = "" ]]; then
     GIT_RELEASE_SHORT_VERSION="BETA"
 else
     GIT_RELEASE_VERSION=$GIT_CURRENT_COMMIT_TAG
 fi
 
 # Set the CFBundleShortVersionString in the generated Info.plist (stripping off the leading \"v\").
-defaults write \"${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}\" \"CFBundleShortVersionString\" \"$GIT_RELEASE_SHORT_VERSION\"
-defaults write \"${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}\" \"CFBundleVersion\" \"$GIT_RELEASE_VERSION\"
+defaults write "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}" "CFBundleShortVersionString" "$GIT_RELEASE_SHORT_VERSION"
+defaults write "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}" "CFBundleVersion" "$GIT_RELEASE_VERSION"
 
-touch \"${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}.plist\"
+touch "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}.plist"
